7583c730599af2db1948cdf7b0fbefe35fe16e74
Johan Sim Jian An <johansja@gmail.com>
2013-10-11 18:35:54 +0800

    Quantities tracked.

---
+++ biza/stocks/admin.py
@@ -0,0 +1,24 @@
+from django.contrib import admin
+
+from .models import *
+
+class WarehouseAdmin(admin.ModelAdmin):
+    list_display = ['name', 'location']
+
+class ItemQuantityInline(admin.TabularInline):
+    model = ItemQuantity
+
+class ItemAdmin(admin.ModelAdmin):
+    list_display = ['name', 'barcode', 'retail_price', 'wholesale_price', 'cost_price']
+    inlines = [ItemQuantityInline,]
+
+class PackageItemInline(admin.TabularInline):
+    model = PackageItem
+
+class PackageAdmin(admin.ModelAdmin):
+    list_display = ['name', 'barcode', 'retail_price', 'separate_retail_price', 'wholesale_price', 'separate_wholesale_price', 'cost_price']
+    inlines = [PackageItemInline,]
+
+admin.site.register(Warehouse, WarehouseAdmin)
+admin.site.register(Item, ItemAdmin)
+admin.site.register(Package, PackageAdmin)
---
+++ biza/stocks/models.py
@@ -0,0 +1,62 @@
+from django.db import models
+
+class Warehouse(models.Model):
+    name = models.CharField(max_length=32)
+    location = models.TextField(unique=True)
+    
+    def __unicode__(self):
+        return self.name
+
+class Product(models.Model):
+    name = models.CharField(max_length=32)
+    barcode = models.CharField(max_length=12)
+    retail_price = models.DecimalField(max_digits=12, decimal_places=4, null=True, blank=True)
+    lowest_retail_price = models.DecimalField(max_digits=12, decimal_places=4, null=True, blank=True)
+    wholesale_price = models.DecimalField(max_digits=12, decimal_places=4, null=True, blank=True)
+    lowest_wholesale_price = models.DecimalField(max_digits=12, decimal_places=4, null=True, blank=True)
+    
+    def __unicode__(self):
+        return self.name
+
+class Item(Product):
+    cost_price = models.DecimalField(max_digits=12, decimal_places=4)
+
+class ItemQuantity(models.Model):
+    item = models.ForeignKey(Item)
+    warehouse = models.ForeignKey(Warehouse)
+    quantity = models.IntegerField()
+    datetime = models.DateTimeField(auto_now=True)
+    
+    class Meta:
+        verbose_name_plural = 'item quantities'
+    
+    def __unicode__(self):
+        return '{} {} at {} on {}'.format(self.quantity, self.item, self.warehouse, self.datetime)
+
+class Package(Product):
+    
+    def cost_price(self):
+        cost = 0
+        for pkgitem in self.packageitem_set.all():
+            cost += pkgitem.quantity * pkgitem.item.cost_price
+        return cost
+    
+    def separate_retail_price(self):
+        price = 0
+        for pkgitem in self.packageitem_set.all():
+            price += pkgitem.quantity * pkgitem.item.retail_price
+        return price
+    
+    def separate_wholesale_price(self):
+        price = 0
+        for pkgitem in self.packageitem_set.all():
+            price += pkgitem.quantity * pkgitem.item.wholesale_price
+        return price
+
+class PackageItem(models.Model):
+    package = models.ForeignKey(Package)
+    item = models.ForeignKey(Item)
+    quantity = models.IntegerField()
+    
+    class Meta:
+        unique_together = ['package', 'item']
