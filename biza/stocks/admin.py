4c009042301964abf5be48be1c7789ff72218f9c
Johan Sim Jian An <johansja@gmail.com>
2013-11-10 13:56:35 +0800

    Added UOM to products.

--- biza/stocks/admin.py
+++ biza/stocks/admin.py
@@ -2,23 +2,33 @@ from django.contrib import admin
 
 from .models import *
 
+
 class WarehouseAdmin(admin.ModelAdmin):
     list_display = ['name', 'location']
 
+
+class UnitOfMeasureAdmin(admin.ModelAdmin):
+    list_display = ['code', 'name']
+
 class ItemQuantityInline(admin.TabularInline):
     model = ItemQuantity
+
 
 class ItemAdmin(admin.ModelAdmin):
     list_display = ['model', 'name', 'barcode', 'retail_price', 'wholesale_price', 'cost_price']
     inlines = [ItemQuantityInline,]
 
+
 class PackageItemInline(admin.TabularInline):
     model = PackageItem
+
 
 class PackageAdmin(admin.ModelAdmin):
     list_display = ['model', 'name', 'barcode', 'retail_price', 'separate_retail_price', 'wholesale_price', 'separate_wholesale_price', 'cost_price']
     inlines = [PackageItemInline,]
 
+
 admin.site.register(Warehouse, WarehouseAdmin)
+admin.site.register(UnitOfMeasure, UnitOfMeasureAdmin)
 admin.site.register(Item, ItemAdmin)
 admin.site.register(Package, PackageAdmin)
