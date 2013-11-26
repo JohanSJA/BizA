11feecbaa539b95d15eb48f684a1c22c966f6934
Johan Sim Jian An <johansja@gmail.com>
2013-11-27 04:03:21 +0800

    Minor edit to login and dashboard page.

--- biza/employees/urls.py
+++ biza/employees/urls.py
@@ -4,5 +4,11 @@ from django.contrib.auth.decorators import login_required
 from django.views.generic import TemplateView
 
 urlpatterns = patterns('',
-    url(r'^dashboard/$', login_required(TemplateView.as_view(template_name='employees/dashboard.html')), name='employees_dashboard'),
+    url(
+        r'^dashboard/$',
+        login_required(
+            TemplateView.as_view(template_name='employees/dashboard.html')
+        ),
+        name='employees_dashboard'
+    ),
 )
