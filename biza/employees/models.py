384e267b257bf579bcfe4fe987283822fad0db9c
Johan Sim Jian An <johansja@gmail.com>
2013-11-27 04:56:01 +0800

    Added company info to user in employee info.

--- biza/employees/models.py
+++ biza/employees/models.py
@@ -1,10 +1,12 @@
 from django.db import models
-
 from django.contrib.auth.models import User
+
+from companies.models import Company
 from retails.models import Store
 
 class Employee(models.Model):
     user = models.OneToOneField(User)
+    company = models.ForeignKey(Company)
     hq = models.BooleanField("HQ access")
     store = models.ForeignKey(Store, null=True, blank=True)
 
