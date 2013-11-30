a35d99015a878f690e014b9b4c4ec2a0fe4a0e9f
Johan Sim Jian An <johansja@gmail.com>
2013-11-30 16:43:10 +0800

    Modify branches app.

--- biza/branches/admin.py
+++ biza/branches/admin.py
@@ -1,10 +1,14 @@
 from django.contrib import admin
 
-from branches.models import Branch
+from branches.models import Branch, Employee
 
+
+class EmployeeInline(admin.StackedInline):
+    model = Employee
 
 class BranchAdmin(admin.ModelAdmin):
-    list_display = ('name', 'manager')
+    list_display = ('name',)
+    inlines = (EmployeeInline,)
 
 
 admin.site.register(Branch, BranchAdmin)
--- biza/branches/models.py
+++ biza/branches/models.py
@@ -5,7 +5,6 @@ from employees.models import Employee
 
 class Branch(models.Model):
     name = models.CharField(max_length=50, unique=True)
-    manager = models.ForeignKey(Employee)
     is_active = models.BooleanField(default=True)
 
     class Meta:
@@ -13,3 +12,10 @@ class Branch(models.Model):
 
     def __unicode__(self):
         return self.name
+
+class Employee(models.Model):
+    branch = models.ForeignKey(Branch)
+    employee = models.ForeignKey(Employee, unique=True)
+
+    def __unicode__(self):
+        return '{} {}'.format(self.branch, self.employee)
