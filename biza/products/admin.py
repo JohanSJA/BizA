6db490190f8ac57c7837ba24dd7f7fcb3f70a5ec
Johan Sim Jian An <johansja@gmail.com>
2013-12-01 11:45:44 +0800

    Removed category and price from products app.

--- biza/products/admin.py
+++ biza/products/admin.py
@@ -1,21 +1,15 @@
 from django.contrib import admin
 
-from products.models import Unit, Category, Product, Component, BasePrice
+from products.models import Unit, Product, Component
 
 
 class ComponentInline(admin.TabularInline):
     model = Component
     fk_name = 'product'
 
-class BasePriceInline(admin.StackedInline):
-    model = BasePrice
-    can_delete = False
-    verbose_name_plural = 'base price'
-
 class ProductAdmin(admin.ModelAdmin):
-    inlines = (ComponentInline, BasePriceInline)
+    inlines = (ComponentInline, )
 
 
 admin.site.register(Unit)
-admin.site.register(Category)
 admin.site.register(Product, ProductAdmin)
--- biza/products/models.py
+++ biza/products/models.py
@@ -8,23 +8,11 @@ class Unit(models.Model):
     def __unicode__(self):
         return self.name
 
-class Category(models.Model):
-    code = models.CharField(max_length=10, unique=True)
-    name = models.CharField(max_length=50, unique=True)
-    description = models.TextField(blank=True)
-
-    class Meta:
-        verbose_name_plural = 'categories'
-
-    def __unicode__(self):
-        return self.name
-
 class Product(models.Model):
     code = models.CharField(max_length=10, unique=True)
     name = models.CharField(max_length=50, unique=True)
     description = models.TextField(blank=True)
     barcode = models.CharField(max_length=30, unique=True)
-    category = models.ForeignKey(Category)
     unit = models.ForeignKey(Unit)
 
     def __unicode__(self):
@@ -37,14 +25,3 @@ class Component(models.Model):
 
     def __unicode__(self):
         return '{} {} in {}'.format(self.quantity, self.component, self.product)
-
-class Price(models.Model):
-    cost = models.DecimalField(max_digits=20, decimal_places=4)
-    base = models.DecimalField(max_digits=20, decimal_places=2)
-    lowest = models.DecimalField(max_digits=20, decimal_places=2)
-
-class BasePrice(Price):
-    product = models.OneToOneField(Product)
-
-    def __unicode__(self):
-        return self.product
