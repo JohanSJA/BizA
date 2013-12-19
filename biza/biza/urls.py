2bcf5f5e03eeac94b37552a7471ccf98bccbc23f
Johan Sim Jian An <johansja@gmail.com>
2013-12-19 18:38:27 +0800

    Ready with retails URLs.

--- biza/biza/urls.py
+++ biza/biza/urls.py
@@ -12,9 +12,9 @@ urlpatterns = patterns('',
     url(r'^$', 'django.contrib.auth.views.login', name='login'),
     url(r'^logout/$', 'django.contrib.auth.views.logout_then_login', name='logout'),
     url(r'^employees/', include('employees.urls')),
-    url(r'^products/', include('products.urls')),
 
-    # url(r'^retails/', include('retails.urls')),
+    url(r'^products/', include('products.urls')),
+    url(r'^retails/', include('retails.urls')),
 
     # Uncomment the admin/doc line below to enable admin documentation:
     url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
