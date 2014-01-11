68152ebffd56b7622c36d160b1668f9054c2c857
Johan Sim Jian An <johansja@gmail.com>
2014-01-11 13:24:52 +0800

    Move all content out of biza sub-folder.

--- biza/settings.py
+++ biza/settings.py
@@ -19,7 +19,7 @@ MANAGERS = ADMINS
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': 'biza',                      # Or path to database file if using sqlite3.
+        'NAME': 'biza_dev',                      # Or path to database file if using sqlite3.
         # The following settings are not used with sqlite3:
         'USER': 'postgres',
         'PASSWORD': 'postgres',
