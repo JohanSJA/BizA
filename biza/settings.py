dfe16315c8c2bb21ef52d27dfc73c250babe0a60
Johan Sim Jian An <johansja@gmail.com>
2014-01-16 19:36:56 +0800

    Ignore South for time being.

--- biza/settings.py
+++ biza/settings.py
@@ -39,7 +39,7 @@ INSTALLED_APPS = (
     'django.contrib.messages',
     'django.contrib.staticfiles',
     # Third-party add-ons / apps
-    'south',
+    # 'south',
     'widget_tweaks',
     # Project apps
     'stocks',
