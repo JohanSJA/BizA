37536792bfee4e9c199ad26698d51054566f3af5
Johan Sim Jian An <johansja@gmail.com>
2014-01-24 11:28:12 +0800

    Use 'extra' field on wholesales formset.

--- wholesales/forms.py
+++ wholesales/forms.py
@@ -3,6 +3,11 @@ from django.forms.models import inlineformset_factory
 from .models import *
 
 
-PartnerLocationFormSet = inlineformset_factory(Partner, Location)
-SaleSaleLineFormSet = inlineformset_factory(Sale, SaleLine)
-PurchasePurchaseLineFormSet = inlineformset_factory(Purchase, PurchaseLine)
+PartnerLocationFormSet = inlineformset_factory(Partner, Location, extra=1)
+
+
+SaleSaleLineFormSet = inlineformset_factory(Sale, SaleLine, extra=5)
+
+
+PurchasePurchaseLineFormSet = inlineformset_factory(Purchase, PurchaseLine,
+        extra=5)
