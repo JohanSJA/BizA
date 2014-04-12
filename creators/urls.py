6a60c73ec500cb72843e8956fc9c123fc7a8741c
Johan Sim Jian An <johansja@gmail.com>
2014-04-12 17:36:57 +0800

    Remove all Django files to start over.

--- creators/urls.py
+++
@@ -1,8 +0,0 @@
-from django.conf.urls import patterns, url
-
-from .views import PartnerWizardView
-
-
-urlpatterns = patterns('',
-    url(r'^partner/new/$', PartnerWizardView.as_view(), name='creators_partner_new'),
-)
