29053d24134866efd9f3c76c3b0d26961a19d30d
Johan Sim Jian An <johansja@gmail.com>
2014-04-12 17:34:49 +0800

    Added product in form.

--- stocks/forms.py
+++ stocks/forms.py
@@ -1,9 +1,10 @@
 from django import forms
 
-from .models import Warehouse
+from .models import Product, Warehouse
 
 
 class ProductLogForm(forms.Form):
+    product = forms.ModelChoiceField(queryset=Product.objects.all())
     warehouse = forms.ModelChoiceField(queryset=Warehouse.objects.all())
     quantity_adjustment = forms.IntegerField(help_text="Positive value for stock increase. Negative value for stock decrease.")
     description = forms.CharField(max_length=100, help_text="Reason for this stock changes to happen.")
